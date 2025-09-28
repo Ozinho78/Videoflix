e7540d9cf0dd9cea4bb21bd65c3329909e17c3a2
Michael Fiebelkorn <michael.fiebelkorn@gmail.com>
2025-09-28 19:41:43 +0200

    simpleworker added

---
+++ video_app/simpleworker.py
@@ -0,0 +1,29 @@
+# Falls bei Windows der Windows RQ Worker nicht funktioniert:
+# 1. Erstelle eine Python Datei names 'simpleworker.py' mit:
+
+from rq import Worker
+
+
+class BaseDeathPenalty(object):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __enter__(self, *args, **kwargs):
+        pass
+
+    def __exit__(self, *args, **kwargs):
+        pass
+
+
+class SimpleWorker(Worker):
+    death_penalty_class = BaseDeathPenalty
+
+    def main_work_horse(self, *args, **kwargs):
+        raise NotImplementedError("Test worker does not implement this method")
+
+    def execute_job(self, *args, **kwargs):
+        """Execute job in same thread/process, do not fork()"""
+        return self.perform_job(*args, **kwargs)
+        
+# 2. Starte den RQ Worker mit diesem Behfehl im Terminal:
+# python manage.py rqworker --worker-class simpleworker.py default
