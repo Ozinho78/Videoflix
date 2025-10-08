8de4974e557f0ea9d88a0d66a8f1d499a8da6c71
Michael Fiebelkorn <michael.fiebelkorn@gmail.com>
2025-10-08 21:55:37 +0200

    reviewing

--- core/utils/exceptions.py
+++ core/utils/exceptions.py
@@ -6,15 +6,9 @@ import logging
 logger = logging.getLogger(__name__)
 
 def exception_handler_status500(exc, context):
-    # Erst DRF-Default versuchen (gibt z. B. 400/404/403 etc. zurück)
+    """Custom exception handler that logs unexpected exceptions and returns HTTP 500"""
     response = exception_handler(exc, context)
-
     if response is None:
-        # Eigene 500-Antwort + kurzer Log
-        # logger.exception(...) würde den Trace loggen; logger.error hält es knapper.
         logger.error('Unexpected error: %s', str(exc))
         return Response({'detail': 'Internal Server Error'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
     return response
-
-
